f703dc59f85d43cb63c8f5c8fd8043472833fc16
oktawave <oktawave@a78ab817-979f-4527-8746-d5ba76819c46>
2013-06-04 09:04:05 +0000

    version 0.7.3
    git-svn-id: svn://svn.code.sf.net/p/oktawave-cli/code/trunk@11 a78ab817-979f-4527-8746-d5ba76819c46

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='oktawave-cli',
-      version='0.7.2',
+      version='0.7.3',
       description='Command line interface to Oktawave',
       author='Oktawave Development Team (by Marek Siemdaj)',
       author_email='support@oktawave.com, marek.siemdaj@gmail.com',
