153788bebea5d1fc0e111f9b16f987050147b9ed
Grzegorz Nosek <root@localdomain.pl>
2013-06-26 18:41:44 +0200

    Slim down dependencies
    No point in pulling the whole swift module with endless dependencies

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(name='oktawave-cli',
       packages=['oktawave'],
       scripts=['oktawave-cli'],
       url='http://oktawave.com',
-      install_requires=['suds', 'swift', 'argparse', 'setproctitle', 'prettytable'],
+      install_requires=['suds', 'python-swiftclient', 'argparse', 'setproctitle', 'prettytable'],
       license='GPLv3',
      )
