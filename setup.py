38397d6d4860330f4947b726d1bf6601a8edf10d
Grzegorz Nosek <root@localdomain.pl>
2013-10-16 21:08:21 +0200

    We're at least on par with upstream 0.7.8, bump version
    Also, update authorship info as this code has no longer almost anything
    in common with the original

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='oktawave-cli',
-      version='0.7.5',
-      description='Command line interface to Oktawave',
-      author='Oktawave Development Team (by Marek Siemdaj)',
-      author_email='support@oktawave.com, marek.siemdaj@gmail.com',
+      version='0.7.8',
+      description='Oktawave API',
+      author='Grzegorz Nosek (based on original code by Marek Siemdaj of Oktawave)',
+      author_email='root@localdomain.pl, support@oktawave.com, marek.siemdaj@gmail.com',
       packages=['oktawave'],
       scripts=['oktawave-cli'],
       url='http://oktawave.com',
